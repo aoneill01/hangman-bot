0d079b1f767905b5ef24ec2c3ee8a58ea2f61bfb
Andy O'Neill <aoneill01@gmail.com>
2020-09-02 19:50:19 -0400

    Shrink image size

--- image.js
+++ image.js
@@ -30,46 +30,46 @@ function updateCanvas(canvas, word, guesses, incorrectCount) {
   for (let i = 0; i < 2; i++) {
     switch (incorrectCount) {
       case 6:
-        // right leg
-        rc.line(50, 90, 25, 135, bodyOptions);
+        // left leg
+        rc.line(37.5, 72.5, 18.75, 106.25, bodyOptions);
         // eyes
-        rc.line(40, 30, 45, 35, bodyOptions);
-        rc.line(40, 35, 45, 30, bodyOptions);
-        rc.line(55, 30, 60, 35, bodyOptions);
-        rc.line(55, 35, 60, 30, bodyOptions);
+        rc.line(30, 27.5, 33.75, 31.25, bodyOptions);
+        rc.line(30, 31.25, 33.75, 27.5, bodyOptions);
+        rc.line(40.25, 27.5, 44, 31.25, bodyOptions);
+        rc.line(40.25, 31.25, 44, 27.5, bodyOptions);
         // mouth
-        rc.line(40, 40, 58, 43, bodyOptions);
+        rc.line(30, 35, 43.5, 37.25, bodyOptions);
       case 5:
         // right leg
-        rc.line(50, 90, 75, 135, bodyOptions);
+        rc.line(37.5, 72.5, 61.25, 106.25, bodyOptions);
       case 4:
         // left arm
-        rc.line(50, 65, 10, 45, bodyOptions);
+        rc.line(37.5, 53.75, 7.5, 38.75, bodyOptions);
       case 3:
         // right arm
-        rc.line(50, 65, 90, 45, bodyOptions);
+        rc.line(37.5, 53.75, 72.5, 38.75, bodyOptions);
       case 2:
         // body
-        rc.line(50, 50, 50, 90, bodyOptions);
+        rc.line(37.5, 42.5, 37.5, 72.5, bodyOptions);
       case 1:
         // head
-        rc.circle(50, 35, 30, bodyOptions);
+        rc.circle(37.5, 31.25, 22.5, bodyOptions);
     }
   }
 
   // gallows
-  rc.line(50, 150, 150, 150, gallowsOptions);
-  rc.line(100, 150, 100, 5, gallowsOptions);
-  rc.line(100, 5, 50, 5, gallowsOptions);
-  rc.line(50, 5, 50, 15, gallowsOptions);
+  rc.line(37.5, 117.5, 112.5, 117.5, gallowsOptions);
+  rc.line(75, 117.5, 75, 8.75, gallowsOptions);
+  rc.line(75, 8.75, 37.5, 8.75, gallowsOptions);
+  rc.line(37.5, 8.75, 37.5, 16.25, gallowsOptions);
 
-  const length = 20;
-  context.font = "28px sans-serif";
+  const length = 18;
+  context.font = "16px sans-serif";
   // blanks
   for (let i = 0; i < word.length; i++) {
-    const margin = 10;
+    const margin = 8;
     const startX = margin + i * (length + margin);
-    rc.line(startX, 195, startX + length, 195);
+    rc.line(startX, 147, startX + length, 147);
     const { width } = context.measureText(word[i].toUpperCase());
     context.fillStyle = "darkblue";
     if (word[i] === word[i].toLowerCase()) {
@@ -79,19 +79,19 @@ function updateCanvas(canvas, word, guesses, incorrectCount) {
       context.fillText(
         word[i].toUpperCase(),
         startX + (length - width) / 2.0,
-        190
+        142
       );
     }
   }
 
   context.fillStyle = "darkblue";
   for (let i = 0; i < guesses.length; i++) {
-    const margin = 30;
+    const margin = 15;
     const perRow = 5;
     const col = i % perRow;
     const row = Math.floor(i / perRow);
-    const startX = 160 + col * (length + margin);
-    const startY = 30 + row * 40;
+    const startX = 130 + col * (length + margin);
+    const startY = 30 + row * 28;
     const { width } = context.measureText(guesses[i].toUpperCase());
     context.fillText(
       guesses[i].toUpperCase(),
@@ -99,13 +99,24 @@ function updateCanvas(canvas, word, guesses, incorrectCount) {
       startY
     );
     if (guesses[i] === guesses[i].toUpperCase()) {
-      rc.circle(startX + length / 2, startY - length / 2, 30, correctOptions);
+      rc.circle(
+        startX + length / 2,
+        startY - length / 2 + 3,
+        20,
+        correctOptions
+      );
     } else {
-      rc.line(startX + length, startY - length, startX, startY, incorrectOptions);
+      rc.line(
+        startX + length,
+        startY - length + 3,
+        startX,
+        startY + 3,
+        incorrectOptions
+      );
     }
   }
 }
 
 module.exports = {
-  updateCanvas
-}
+  updateCanvas,
+};
